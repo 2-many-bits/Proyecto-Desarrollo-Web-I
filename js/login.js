b6e782c0c91717500894cac7d3877512becf32be
Armando Salgado <20245138@esen.edu.sv>
2025-11-19 15:02:10 -0600

    Login y crear usuario funcionales

---
+++ js/login.js
@@ -0,0 +1,24 @@
+import { loginUser } from "./firebase/firebase_config.js";
+
+const inputTxtEmail = document.getElementById("idTxtCorreo");
+const inputTxtPassword = document.getElementById("idTxtContrasenia");
+
+document.getElementById("loginForm").addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const email = inputTxtEmail.value;
+    const password = inputTxtPassword.value;
+
+    try {
+        const userData = await loginUser (email, password);
+
+        if (userData) {
+            window.location.href = "../index.html";
+        } else {
+            alert("Usuario y/o contraseña incorrectos");
+        }
+    } catch (error) {
+        alert("Hubo un problema al iniciar sesión");
+        console.error("Error al inciar sesión: ", error.message);
+    }
+});
